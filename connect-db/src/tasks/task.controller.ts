c8cd114778377ad8f7f24c7fc0b8fb49b036e377
Jp12game <jos.pol.h@gmail.com>
2026-05-27 23:13:38 -0600

    Task Controller completo

---
+++ connect-db/src/tasks/task.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create(
+    @Body()
+    body: {
+      titulo: string;
+      userId: number;
+      categoriaId: number;
+      descripcion?: string;
+    },
+  ): Promise<Task> {
+    return this.tasksService.createTask(
+      body.titulo,
+      body.userId,
+      body.categoriaId,
+      body.descripcion,
+    );
+  }
+
+  @Get()
+  findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.findById(+id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: Partial<Task>,
+  ): Promise<Task> {
+    return this.tasksService.updateTask(+id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this.tasksService.deleteTask(+id);
+  }
+}
--- connect-db/src/tasks/tasks.module.ts
+++ connect-db/src/tasks/tasks.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
+import { TasksController } from './task.controller';
 import { Task } from './task.entity';
 import { User } from '../users/user.entity';
 import { Category } from '../categories/category.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task, User, Category])],
+  controllers: [TasksController],
   providers: [TasksService],
   exports: [TypeOrmModule],
 })
